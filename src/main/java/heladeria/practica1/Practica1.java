3c83697128c95d791cf8cd1b4c019cde40cd378c
Marlon Bejarano Bolanos <abb@fabmation.de>
2025-10-17 22:00:46 +0200

    first commit

---
+++ src/main/java/heladeria/practica1/Practica1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package heladeria.practica1;
+
+/**
+ *
+ * @author abb
+ */
+public class Practica1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
